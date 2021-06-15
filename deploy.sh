c6cbd4fcabb7ad3eb27a901190e7b1925087219c
frenchiecoin <frenchie@governordao.org>
2021-06-16 01:36:15 +1000

    Make warning message conditional

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-npm install
+npm install -f
 npm uninstall governor-common
-npm install governor-common@git+https://github.com/Governor-DAO/governor-common-private.git
+npm install governor-common@git+https://github.com/Governor-DAO/governor-common-private.git -f
 RANDDIR="temp_build_$(openssl rand -hex 12)"
 BUILD_PATH=$RANDDIR npm run build
 if [ -f "$RANDDIR/index.html" ]; then
